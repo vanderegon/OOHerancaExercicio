63897e44b7112f5f36f8633bcaae9dd553c53133
Cristian Paes <67326386+CrisPaes30@users.noreply.github.com>
2020-11-16 16:05:04 -0300

    Update ContaPoupanca.java
    Alteração no calculo poupança

--- src/ContaPoupanca.java
+++ src/ContaPoupanca.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
+
 public class ContaPoupanca extends Conta {
+
+
     private int diaAniversario;
-    private double  taxaDeJuros;
+    private double taxaDeJuros;
+
 
     public ContaPoupanca(int numero, int agencia, String banco, double saldo, int diaAniversario, double tacaDeJuros) {
         super(numero, agencia, banco, saldo);
@@ -8,7 +13,23 @@ public class ContaPoupanca extends Conta {
         this.taxaDeJuros = tacaDeJuros;
     }
 
-    public double getSaldo(){
+    public double getSaldo() {
         return this.saldo + this.taxaDeJuros * this.saldo;
     }
+
+    public void ValorDepositoPoupanca() {
+
+        System.out.println("Informe o valor de investimento R$ ");
+
+        Scanner scanner = new Scanner(System.in);
+        Double valorDeposito = scanner.nextDouble();
+
+        double valorDep = saldo += valorDeposito;
+
+        if (saldo >= valorDeposito) {
+            System.out.println("Seu novo Saldo R$  " + valorDep);
+            System.out.println("Saldo mais taxa R$ " + this.saldo * this.taxaDeJuros);
+        }
+    }
 }
+
